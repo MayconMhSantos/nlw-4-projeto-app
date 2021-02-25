4d2d607799bf61acf6f7c1fa31a68641091dfeb4
Maycon Santos <68924739+MayconMhSantos@users.noreply.github.com>
2021-02-25 00:07:39 -0300

    up
    Aula 3  NLW#

--- src/components/ExperienceBar.tsx
+++ src/components/ExperienceBar.tsx
@@ -1,18 +1,24 @@
 
+import { useContext } from 'react'
+import { ChallengesContext } from '../contexts/ChallengesContexts'
 import style from '../styles/components/ExperienceBar.module.css'
 
 export function ExperienceBar() {
+    const { currentExperience, experienceToNextLevel } = useContext(ChallengesContext);
+
+    const percentToNextLevel = Math.round(currentExperience * 100) / experienceToNextLevel;
+
     return (
         <header className={style.experienceBar}> 
             <span>0 px</span>
             <div>
-                <div style={{ width: '50%'}} />
+                <div style={{ width: `${percentToNextLevel}%`}} />
 
-                <span className={style.currentExperience} style={{ left: '50%'}}>
-                    300 xp
+                <span className={style.currentExperience} style={{ left: `${percentToNextLevel}%`}}>
+                    {currentExperience} xp
                 </span>
             </div>
-            <span>600 px</span>
+            <span>{experienceToNextLevel} px</span>
         </header>
     )
 }
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,16 @@
 // Este conteudo ira se repetir em todas as paginas
 import '../styles/global.css'
 
+import { ChallengesProvider } from '../contexts/ChallengesContexts'
+
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
-}
+
+  return (
+
+  <ChallengesProvider>
+  <Component {...pageProps} />
+  </ChallengesProvider >
+
+  )}
 
 export default MyApp
